b6b89d0c24d8abb0272413b064059186e9a2b121
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-07 13:15:57 +0000

    H

--- Visualization/sample.js
+++ Visualization/sample.js
@@ -17,7 +17,7 @@ looker.plugins.visualizations.add({
       // console.log("details",details)
 
 
-    var chart = element.table;
+    var chart = element.firstChild;
     chart.innerHTML = '';
     console.log(chart);
 
