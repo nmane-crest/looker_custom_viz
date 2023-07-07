59534a2a903debc45998dab966407b6d0ddecf70
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-07 09:56:22 +0000

    changes added

--- Visualization/sample.js
+++ Visualization/sample.js
@@ -1,8 +1,8 @@
 looker.plugins.visualizations.add({
     create: function (element, config) {
         // element.innerHTML = "<h1>Ready to render!</h1>";
-        var chart = document.createElement('div');
-        chart.id = 'custom-histogram-chart';
+        var chart = document.createElement('table');
+        chart.id = 'custom-table-chart';
         element.appendChild(chart);
         // const para = document.createElement("p");
         // para.innerText = "This is a paragraph";
@@ -10,14 +10,18 @@ looker.plugins.visualizations.add({
     },
     updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
         var html = "";
-        // for (var row of data) {
-        //     var cell = var cell = row[queryResponse.fields.dimensions[0].name];
-        //     html += LookerCharts.Utils.htmlForCell(cell);
-        // }
-
-        for (var i of queryResponse.fields.dimensions){
-          html +=i.name
+        for (var row of data) {
+          for (var i of queryResponse.fields.dimensions){
+            // var cell = row[i];
+             html += "<table><tr><td>row[i]</td></table>";
+            html += LookerCharts.Utils.htmlForCell(cell);
         }
+    }
+        // for (var i of queryResponse.fields.dimensions){
+        //   html +=i.name
+        // }
+      // html += "<table><tr><td>123</td><td>456</td></tr></table>";
+
        element.innerHTML = html;
         doneRendering()
     }
