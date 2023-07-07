95423cea5c7e2d1fe4df6266865041d997fda961
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-07 10:12:46 +0000

    some modification

--- Visualization/sample.js
+++ Visualization/sample.js
@@ -9,19 +9,41 @@ looker.plugins.visualizations.add({
         // document.body.appendChild(para);
     },
     updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
-        var html = "";
-        for (var row of data) {
+        var data = []
+        for (var d of data) {
+          var row={}
           for (var i of queryResponse.fields.dimensions){
-            var cell = row[i.name];
-            html += LookerCharts.Utils.htmlForCell(cell);
+            var cell = d[i.name];
+            row[i.name] = d[i.name]
+            // html += LookerCharts.Utils.htmlForCell(cell);
         }
+        data.push(cell)
     }
-        // for (var i of queryResponse.fields.dimensions){
-        //   html +=i.name
-        // }
-      // html += "<table><tr><td>123</td><td>456</td></tr></table>";
-
-       element.innerHTML = html;
-        doneRendering()
+    element.innerHTML = data
+    doneRendering()
     }
 });
+
+function table_creation(){
+    // Create table header
+    var headerRow = document.createElement('tr');
+    Object.keys(data[0]).forEach(function(key) {
+      var th = document.createElement('th');
+      th.textContent = key;
+      headerRow.appendChild(th);
+    });
+    chart.appendChild(headerRow);
+
+    // Create table rows
+    data.forEach(function(item) {
+      var row = document.createElement('tr');
+      Object.values(item).forEach(function(value) {
+        var td = document.createElement('td');
+        td.textContent = value;
+        row.appendChild(td);
+      });
+      chart.appendChild(row);
+    });
+
+    element.appendChild(chart);
+}
