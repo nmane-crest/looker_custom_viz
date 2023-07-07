024b2a8942a4e271612da0f6319b095f68a66c6a
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-07 10:24:24 +0000

    ergg

--- Visualization/sample.js
+++ Visualization/sample.js
@@ -3,24 +3,44 @@ looker.plugins.visualizations.add({
         // element.innerHTML = "<h1>Ready to render!</h1>";
         var chart = document.createElement('table');
         chart.id = 'custom-table-chart';
-        element.appendChild(chart);
+        // element.appendChild(chart);
         // const para = document.createElement("p");
         // para.innerText = "This is a paragraph";
         // document.body.appendChild(para);
     },
     updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
-        var data = []
-        for (var d of data) {
-          // var row={}
-          for (var i of queryResponse.fields.dimensions){
-            // var cell = d[i.name];
-            // row[i.name] = d[i.name]
-            data.push(d[i.name])
-            // html += LookerCharts.Utils.htmlForCell(cell);
-        }
-        // data.push(cell)
-    }
-    element.innerHTML = data.toString()
+    //     var data = []
+    //     for (var d of data) {
+    //       // var row={}
+    //       for (var i of queryResponse.fields.dimensions){
+    //         // var cell = d[i.name];
+    //         // row[i.name] = d[i.name]
+    //         data.push(d[i.name])
+    //         // html += LookerCharts.Utils.htmlForCell(cell);
+    //     }
+    //     // data.push(cell)
+    // }
+    // Create table header
+    var headerRow = document.createElement('tr');
+    Object.keys(queryResponse.fields.dimensions).forEach(function(key) {
+      var th = document.createElement('th');
+      th.textContent = key;
+      headerRow.appendChild(th);
+    });
+    chart.appendChild(headerRow);
+    // // Create table rows
+    // data.forEach(function(item) {
+    //   var row = document.createElement('tr');
+    //   Object.values(item).forEach(function(value) {
+    //     var td = document.createElement('td');
+    //     td.textContent = value;
+    //     row.appendChild(td);
+    //   });
+    //   chart.appendChild(row);
+    });
+
+    element.appendChild(chart);
+    // element.innerHTML = data.toString()
     doneRendering()
     }
 });
