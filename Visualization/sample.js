c5927378a986aa16944b9891c4f881ac090796e8
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-07 11:44:17 +0000

    wefefe

--- Visualization/sample.js
+++ Visualization/sample.js
@@ -1,11 +1,4 @@
 looker.plugins.visualizations.add({
-    create: function (element, config) {
-      console.log(" create started......")
-        // var chart = document.createElement('table');
-        // chart.id = 'custom-table-chart';
-        // element.appendChild(chart);
-        console.log("create End ...")
-    },
     updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
     var chart = document.createElement('table');
     chart.id = 'custom-table-chart';
@@ -13,7 +6,7 @@ looker.plugins.visualizations.add({
     // Create table header
     var headerRow = document.createElement('tr');
     for (var i of queryResponse.fields.dimensions){
-      console.log("Dimansion name:",i.name)
+      // console.log("Dimansion name:",i.name)
       var th = document.createElement('th');
       th.textContent = i.name;
       headerRow.appendChild(th);
@@ -22,18 +15,17 @@ looker.plugins.visualizations.add({
     console.log("headerRow completed...")
     // Create table rows
     var row = document.createElement('tr');
-    data.forEach(function(value) {
-      console.log("Value:",value)
-      for (var i of queryResponse.fields.dimensions){
-        var td = document.createElement('td');
-        console.log("actul value:",value[i.name].value)
-        td.textContent = value[i.name].value;
-        row.appendChild(td);
+    for (var i of queryResponse.fields.dimensions){
+      console.log("i:",i.name)
+      // data.forEach(function(value) {
+      var td = document.createElement('td');
+      console.log("actul value:",data[i.name].value)
+      td.textContent = data[i.name].value;
+      row.appendChild(td);
       }
       console.log("row added...")
       chart.appendChild(row);
       console.log("successfully appended....")
-    });
     element.appendChild(chart);
     console.log("chart added.....")
     doneRendering()
