1250ad3911ad5c1709fa969194f99fb1ccd9641a
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-07 09:46:33 +0000

    rfwe

--- Visualization/sample.js
+++ Visualization/sample.js
@@ -1,6 +1,12 @@
 looker.plugins.visualizations.add({
     create: function (element, config) {
-        element.innerHTML = "<h1>Ready to render!</h1>";
+        // element.innerHTML = "<h1>Ready to render!</h1>";
+        var chart = document.createElement('div');
+        chart.id = 'custom-histogram-chart';
+        element.appendChild(chart);
+        // const para = document.createElement("p");
+        // para.innerText = "This is a paragraph";
+        // document.body.appendChild(para);
     },
     updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
         var html = "";
@@ -12,8 +18,7 @@ looker.plugins.visualizations.add({
         for (var i of queryResponse.fields.dimensions){
           html +=i.name
         }
-
-        element.innerHTML = html;
+       element.innerHTML = html;
         doneRendering()
     }
 });
