05993421f6f9f73db748e8e2f01cc4147001c78e
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-07 11:00:06 +0000

    efwefewf

--- Visualization/sample.js
+++ Visualization/sample.js
@@ -1,73 +1,39 @@
 looker.plugins.visualizations.add({
     create: function (element, config) {
-        element.innerHTML = "<h1>Ready to render!</h1>";
-        // var chart = document.createElement('table');
-        // chart.id = 'custom-table-chart';
+      console.log(" create started......")
+        var chart = document.createElement('table');
+        chart.id = 'custom-table-chart';
         // element.appendChild(chart);
-        // const para = document.createElement("p");
-        // para.innerText = "This is a paragraph";
-        // document.body.appendChild(para);
+        console.log("create End ...")
     },
     updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
-    //     var data = []
-    //     for (var d of data) {
-    //       // var row={}
-    //       for (var i of queryResponse.fields.dimensions){
-    //         // var cell = d[i.name];
-    //         // row[i.name] = d[i.name]
-    //         data.push(d[i.name])
-    //         // html += LookerCharts.Utils.htmlForCell(cell);
-    //     }
-    //     // data.push(cell)
-    // }
+      console.log("update started.....")
     // Create table header
-    var html=""
-    // var headerRow = document.createElement('tr');
+    var headerRow = document.createElement('tr');
     for (var i of queryResponse.fields.dimensions){
-      // var th = document.createElement('th');
-      // th.textContent = key;
-      // headerRow.appendChild(th);
-      html+=LookerCharts.Utils.htmlForCell(i);
-i
+      console.log("Dimansion name:",i)
+      var th = document.createElement('th');
+      th.textContent = i;
+      headerRow.appendChild(th);
     }
-    // chart.appendChild(headerRow);
-    // // Create table rows
-    // data.forEach(function(item) {
-    //   var row = document.createElement('tr');
-    //   Object.values(item).forEach(function(value) {
-    //     var td = document.createElement('td');
-    //     td.textContent = value;
-    //     row.appendChild(td);
-    //   });
-    //   chart.appendChild(row);
-    // });
-
+    chart.appendChild(headerRow);
+    console.log("headerRow completed...")
+    // Create table rows
+    var row = document.createElement('tr');
+    data.forEach(function(value) {
+      console.log("Value:",value)
+      for (var i of queryResponse.fields.dimensions){
+        var td = document.createElement('td');
+        console.log("actul value:",value[i.name])
+        td.textContent = value[i.name];
+        row.appendChild(td);
+      }
+      console.log("row added...")
+      chart.appendChild(row);
+      console.log("successfully appended....")
+    }
     element.appendChild(chart);
-    element.innerHTML = html
+    console.log("chart added.....")
     doneRendering()
     }
 });
-
-// function table_creation(){
-//     // Create table header
-//     var headerRow = document.createElement('tr');
-//     Object.keys(data[0]).forEach(function(key) {
-//       var th = document.createElement('th');
-//       th.textContent = key;
-//       headerRow.appendChild(th);
-//     });
-//     chart.appendChild(headerRow);
-
-//     // Create table rows
-//     data.forEach(function(item) {
-//       var row = document.createElement('tr');
-//       Object.values(item).forEach(function(value) {
-//         var td = document.createElement('td');
-//         td.textContent = value;
-//         row.appendChild(td);
-//       });
-//       chart.appendChild(row);
-//     });
-
-//     element.appendChild(chart);
-// }
