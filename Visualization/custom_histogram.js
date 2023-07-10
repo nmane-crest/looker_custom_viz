4ddbf6ebaeb8452986786478569f4e0811d3d4c0
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-10 08:22:25 +0000

    wqdch

--- Visualization/custom_histogram.js
+++ Visualization/custom_histogram.js
@@ -63,6 +63,9 @@ function updateHistogram(element, data) {
     console.log("update Histogram started..")
     // const d3 = window.d3;
     var svg = d3.select(element);
+    var xScale = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, 400]);
     var bars = svg.selectAll('rect')
         .data(data)
         .attr('width', function (d) {
