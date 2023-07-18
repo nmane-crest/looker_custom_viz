f3ca96cab53f77a1e5cf1b158e35975a05b561ac
Nikhil Mane <nikhil.mane@crestdatasys.com>
2023-07-18 07:01:04 +0000

    erfejwyv

--- check.js
+++ check.js
@@ -16,47 +16,37 @@ looker.plugins.visualizations.add({
     },
 
     updateAsync: function (data, element, config, queryResponse, details, done) {
-        // console.log(queryResponse)
         var fieldData = queryResponse.fields.dimensions.map(dim => dim.name);
         var measuresData = queryResponse.fields.measures.map(mes => mes.name);
 
         var fields = []
         fields.push(fieldData[0])
         fields.push(measuresData[0])
-        // console.log(fields)
         // Create a set to keep track of unique field values
         const data_dim_mes=[]
 
         // Loop through each row of data and add field values to the set
-        console.log(data)
         data.forEach(row => {
             fields.forEach(field =>{
                 data_dim_mes.push(row[field].value)
             });
         });
-        console.log(data_dim_mes)
-        // Convert the set to an array and sort it (optional)
-        // const uniqueValues = Array.from(uniqueValuesSet).sort();
-
-        // Clear the existing content of the container
-        // this.container.innerHTML = "";
 
         // Create a div element to hold the unique field data
         const fieldDataElement = document.createElement("div");
         fieldDataElement.style.display = "flex";
-        fieldDataElement.style.flexDirection = "column"; // Display elements below each other
+        fieldDataElement.style.flexDirection = "row"; // Display elements below each other
         fieldDataElement.style.alignItems = "center"; // Center elements horizontally
 
         // Loop through each unique field value and create a div for each
         data_dim_mes.forEach(value => {
             const fieldElement = document.createElement("div");
-            fieldElement.textContent = value;
+            fieldElement.textContent = value + ":" ;
             fieldDataElement.appendChild(fieldElement);
         });
 
         // Append the unique field data div to the visualization container
         this.container.appendChild(fieldDataElement);
-
         done();
     }
 });
